16c52eab80ebd50d8cc0388c075e48cb9dc8dd43
Daniel Alves de Moraes <daniel.alves@conexasaude.com.br>
2022-12-08 16:46:48 -0300

    mapeando user com permission

--- src/main/java/com/end/beck/desafio/imedback/Model/Permission.java
+++ src/main/java/com/end/beck/desafio/imedback/Model/Permission.java
@@ -2,15 +2,24 @@ package com.end.beck.desafio.imedback.Model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.Data;
 
 
 @Data
 @Entity
-public class Permission  implements Serializable{
+public class Permission{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private long id;
+   
+    @Column
     private String description;    
 }
