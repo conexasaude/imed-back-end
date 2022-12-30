babcc976e8a2cd3bbe840a71a3a38cf246326c6a
Daniel Alves de Moraes <daniel.alves@conexasaude.com.br>
2022-12-30 14:54:14 -0300

    alterano retorno getAuthorities

--- src/main/java/com/end/beck/desafio/imedback/Model/User.java
+++ src/main/java/com/end/beck/desafio/imedback/Model/User.java
@@ -71,7 +71,7 @@ public class User implements UserDetails {
   
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return  new ArrayList<>();
+        return  this.permissions;
     }
 
     @Override
